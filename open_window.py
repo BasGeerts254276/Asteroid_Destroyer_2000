ea006ed34312628c42cd429db9ffab8fb5ea20fb
BasGeerts254276 <254276@buas.nl>
2025-11-05 15:44:51 +0100

    feature complete

--- open_window.py
+++ open_window.py
@@ -1,4 +1,42 @@
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 850
-SCREEN_TITLE = "Asteroid destroyer 2000"
+import arcade
 
+# Constants
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Asteroid Destroyer 2000"
+
+
+class GameView(arcade.Window):
+    
+    def __init__(self):
+
+        # Call the parent class to set up the window
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
+        self.background_color = arcade.csscolor.BLACK
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        pass
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # The clear method should always be called at the start of on_draw.
+        # It clears the whole screen to whatever the background color is
+        # set to. This ensures that you have a clean slate for drawing each
+        # frame of the game.
+        self.clear()
+
+        # Code to draw other things will go here
+
+
+def main():
+    """Main function"""
+    window = GameView()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
